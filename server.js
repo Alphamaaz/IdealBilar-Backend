a6a532e9cd83541f65fd5e4a239fa65285ec6927
Maaz Khan <hamadkhan1520@gmail.com>
2026-04-07 12:23:57 +0500

    added basic setup

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const App = require("./src/app");
+const { default: connectDB } = require("./src/database/db");
+
 
 App.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+connectDB();
