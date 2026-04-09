bae42776fae2ebfd76017edc5500b9024b947a3d
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2026-04-09 16:37:48 +0500

    This is the user controller
    in this user.controller.js file we have multiple controllers for different purpose's. we have total five controller in this file, one is userRegisterController for user registration, then we have userLoginController which we use for the user login, then we have userForgetPasswordController through this we handle the forget passwrod request response. one is OTPVerifyController use for OTP verifyin request, response, then we have the userRestPasswordController use for the reset password request, response

--- src/modules/user/controllers/user.controller.js
+++ src/modules/user/controllers/user.controller.js
@@ -1,7 +1,127 @@
-const userController = (req , res) => {
-    res.status(200).json({
-        message: "we reached to the user endpoint!"
-    })
-}
+//External modules
 
-module.exports = { userController };
+//Internal modules
+const userService = require("../services/user.service");
+const settingResponse = require("../utils/settingResponse");
+
+// User registration controller
+const userRegisterController = async (req, res) => {
+  try {
+    const result = await userService.userRegister(req.body);
+    
+    if (result instanceof Error) {
+      return settingResponse(res, result);
+    }
+
+    
+    if(result.code === 11000){
+      return res.status(409).json(result)
+    }
+
+    res.status(201).json({
+      message: "User registered successfuly!",
+      data: result,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: err.message,
+    });
+  }
+};
+
+// User login controller
+const userLoginController = async (req, res) => {
+  try {
+    const result = await userService.userLogin(req.body);
+    if (result instanceof Error) {
+      return settingResponse(res, result);
+    }
+
+    if(!result.success){
+      return res.status(401).json(result);
+    }
+    
+    res.status(200).json({
+      message: "Login successfuly!",
+      data: result,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: err.message,
+    });
+  }
+};
+
+// user forget password controller
+const userForgetPasswordController = async (req, res) => {
+  try {
+    const result = await userService.userForgetPassword({
+        id: req.user._id,
+        name: req.user.name,
+        email: req.user.email,
+    });
+
+    res.status(200).json({
+      success: true,
+      data: result,
+    });
+  } catch (err) {
+    return new Error(err.message);
+  }
+};
+
+// user OTP verify controller
+const OTPVerifyController = async (req, res) => {
+  try { 
+    const result = await userService.userOTPVerify({
+        userId: req.userId,
+        otp: req.headers.otp,
+    });
+    
+    if (result instanceof Error) {
+      return settingResponse(res, result);
+    }
+    res.status(200).json({
+      success: true,
+      data: result,
+    });
+
+  } catch (err) {return new Error(err.message);} 
+};
+
+//  user reset password controller
+const userResetPasswordController = async (req, res) => {
+  try {
+    const { newPassword, confirmPassword } = req.body;
+    const userId = req.userId;
+    let result = null;
+    try {
+     result = await userService.userResetPassword({ newPassword, confirmPassword, userId });
+    }
+    catch (err) {
+      return settingResponse(res, err);
+     }
+    if (result instanceof Error) {
+      return settingResponse(res, result);
+    } 
+
+    res.status(200).json({
+      success: true,
+      message: "Password reset successfully",
+      result,
+    });
+   
+  } catch (err) {
+    return new Error(err.message);
+  }
+};
+//Exports
+module.exports = {
+  userRegisterController,
+  userLoginController,
+  userForgetPasswordController,
+  OTPVerifyController,
+  userResetPasswordController
+};
