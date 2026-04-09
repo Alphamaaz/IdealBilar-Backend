eee173871b07cfbef284500d8ea10375521abad2
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2026-04-09 16:47:14 +0500

    Create the user.middlewares.js file in this we have all the middlewares which is used in the user module, in this we have total three middlewares, one is userMiddlewareForVerifyJwtToken use to verify the jwt token, then we have authenticateUserForForgetPassword use to authenticate the user which forget their password. one is OTPVerifyMiddleware use for to verify the OTP of the user

---
+++ src/modules/user/middlewares/user.middlewares.js
@@ -0,0 +1,72 @@
+//Internal modules
+const jwtVerify = require("../../../shared/utils/jwtVerify");
+const userService = require("../services/user.service");
+const settingResponse = require("../utils/settingResponse");
+
+// middleware for verifying the user jwt token
+const userMiddlewareForVerifyJwtToken = (req, res, next) => {
+  try {
+    const token = req.headers.token;
+
+    if (!token) {
+      return res
+        .status(401)
+        .json({ success: false, error: "Token is required!" });
+    }
+    const { success, error, data } = jwtVerify(token);
+
+    req.userId = data;
+
+    if (error) {
+      return res
+        .status(400)
+        .json({ success: false, error: `Token is invalid!` });
+    }
+
+    next();
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+};
+
+// middleware for authenticate user, when he/she/other forget their password
+const authenticateUserForForgetPassword = async (req, res, next) => {
+  try {
+    const result = await userService.userData(req.body);
+
+    if (result instanceof Error) {
+      return settingResponse(res, result);
+    }
+    req.user = result;
+    next();
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+// OTP verify middleware
+const OTPVerifyMiddleware = async (req, res, next) => {
+  try {
+    const userId = req.userId;
+    const otp = req.headers.otp;
+    
+    const result = await userService.userOTPVerify({ userId, otp });  
+    if (result instanceof Error) {
+      return res.status(result.status).json({
+        success: false,
+        error: result.message,
+      });
+    }
+    req.otpVerifiedUser = result;
+    next();
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+// exports
+module.exports = {
+  userMiddlewareForVerifyJwtToken,
+  authenticateUserForForgetPassword,
+  OTPVerifyMiddleware
+};
