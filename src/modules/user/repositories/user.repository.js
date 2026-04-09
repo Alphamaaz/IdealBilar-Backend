8a4dd0cbcc20692e3c04a770f1109a95978da770
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2026-04-09 17:00:49 +0500

    Created repository directory which contain all the database logic, and through this we direct communicate to the DB. in this we have multiple fnction's like for creating new user we have the createUser function, then ftching user data using email for this we have the getUserByEmail function, then for saving the OTP in DB we have the userOTPSave function, then verify the OTP in the Database eginne for this we have the OTPVerify function, and for saving the user rest password we have the userResetPassword

---
+++ src/modules/user/repositories/user.repository.js
@@ -0,0 +1,61 @@
+//Internal modules
+const User = require('../models/user.model');
+const userOTPVerifyMdoel = require('../models/user.OTPVerify.model');
+const mongoose = require('mongoose');
+// Create a new user
+const createUser = async (userData) => {
+    try {
+        const user = await User.create(userData);
+        return user;
+        
+    } catch(err) {
+        throw err;
+    }
+};
+
+// Get user by email
+const getUserByEmail = async (email) => {
+    try {
+        const user = await User.findOne({ email });
+        return user;
+    } catch (err) {
+        throw new Error(err.message);
+    }
+};
+
+// save OTP to user
+const userOTPSave = async (userId, otp) => {
+    try {
+        const user = await userOTPVerifyMdoel.findOneAndUpdate({ userId: new mongoose.Types.ObjectId(userId) }, { otp, expireAt: Date.now() + 5 * 60 * 1000 }, { returnDocument: 'after', upsert: true });
+        return user;
+    } catch (err) {
+        throw new Error(err.message);
+    }   
+};
+
+// OTPVerify 
+const OTPVerify = async (userId, otp) => {
+    try {
+        const user = await userOTPVerifyMdoel.findOne({ userId: new mongoose.Types.ObjectId(userId), otp });
+        return user;
+    } catch (err) {
+        console.log("We are in the OTPVerify repository");
+        throw new Error(err.message);
+    }
+};
+
+// user reset password save
+const userResetPassword = async (userId, newPassword) => {
+    try {
+        const user = await User.findByIdAndUpdate(userId, { password: newPassword }, { returnDocument: 'after' });
+        return user;
+    } catch (err) {
+        console.log("We are in the user reset password repository ", err.message);
+        throw new Error(err.message);
+    }
+};
+
+
+
+// exports
+module.exports = { createUser, getUserByEmail, userOTPSave, OTPVerify, userResetPassword };
