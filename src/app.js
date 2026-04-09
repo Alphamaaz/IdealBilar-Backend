e3a5b30ababc27589f9f50c84d5cc7462e777685
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2026-04-09 17:55:48 +0500

    This is the main server for the mini servers/routers

--- src/app.js
+++ src/app.js
@@ -2,9 +2,21 @@
 const express = require('express');
 //Internal modules
 const {userRouter} = require("./modules/user");
+const clientTestimonialRoutes = require('./modules/clientTestimonials');
+const RentACarRouter = require('./modules/rentACar')
 
 const app = express();
 
+app.use(express.json())
+
+// User URL's
 app.use('/api',userRouter);
+
+// Client testimonial URL's
+app.use('/api', clientTestimonialRoutes);
+
+//Rent a car URL's
+
+app.use('/api',RentACarRouter);
 
  module.exports = app;
