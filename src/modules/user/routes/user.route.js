083f5cd85f42c6d3ed7d3a3d8d7fe5118f1805a8
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2026-04-09 17:04:50 +0500

    Created the routes directory for router
    which contain all the endpoints/URL's of user. endPoints like user-signup, user-signin, forget-password, otp-verify, rest-password

--- src/modules/user/routes/user.route.js
+++ src/modules/user/routes/user.route.js
@@ -1,8 +1,46 @@
 const express = require("express");
-const { userController: userHandler } = require("../controllers/user.controller");
-
+const {
+  userRegisterController: userRegisterHandler,
+  userLoginController: userLoginHandler,
+  userForgetPasswordController: userForgetPasswordHandler,
+  OTPVerifyController: OTPVarifyHandler,
+  userResetPasswordController: userResetPasswordHandler
+} = require("../controllers/user.controller");
+const {
+  userMiddlewareForVerifyJwtToken,
+  authenticateUserForForgetPassword,
+  OTPVerifyMiddleware,
+} = require("../middlewares/user.middlewares");
 const Router = express.Router();
 
-Router.get('/user', userHandler)
+// user sign up endpoint
+Router.post("/user-signup", userRegisterHandler);
 
+// user sign in endpoint
+Router.post("/user-signin", userLoginHandler);
+
+// user forget password endpoint
+Router.post(
+  "/forget-password",
+  userMiddlewareForVerifyJwtToken,
+  authenticateUserForForgetPassword,
+  userForgetPasswordHandler,
+);
+
+// user reset password endpoint
+Router.post(
+  "/reset-password",
+    userMiddlewareForVerifyJwtToken,
+    userResetPasswordHandler
+);
+
+// user OTP verify endpoint
+Router.post(
+  "/otp-verify",
+  userMiddlewareForVerifyJwtToken,
+  OTPVerifyMiddleware,
+  OTPVarifyHandler,
+);
+
+// Export
 module.exports = Router;
