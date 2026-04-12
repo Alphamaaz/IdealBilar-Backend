9de0b2a332bb90cff62a5b19ea6bb58a8fc95bf8
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2026-04-12 22:39:42 +0500

    This controller is for delete the contact us request, and response

--- src/modules/contactus/controllers/contactUsDelete.controller.js
+++ src/modules/contactus/controllers/contactUsDelete.controller.js
@@ -1,14 +1,22 @@
-// External modules
-
 // Internal modules
+import { success } from "zod";
+import settingErrorStatusAndMessage from "../../../shared/utils/settingErrorStatusAndMessage.js";
+import { contactUsDeleteService } from "../services/contactUsDelete.service.js";
 const contactUsDeleteController = async (req, res) => {
     try{
-         const userId = req.params.userId;
-         console.log("User Id : ", userId);
+         const result = await contactUsDeleteService(req.params.userId);
          
+         if(!result){
+            return res.status(404).json({
+                success: false,
+                message: "Record is not found!"
+            })
+         }
+
         res.status(200).json({
             success: true,
-            message: "Reached to the contact us delete controller!"
+            message: `Contact us of ${result.firstName}, and his/her complaint about ${result.subject} delete successfully!`,
+            result
         })
     }catch(err){
         throw err;
