05cc176f331da1f0d47088551040bff2391bc0d4
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2026-04-14 22:22:24 +0500

    Update user middleware

--- src/modules/user/middlewares/user.middlewares.js
+++ src/modules/user/middlewares/user.middlewares.js
@@ -3,26 +3,6 @@ import * as userService from "../services/user.service.js";
 import settingResponse from "../../../shared/utils/settingResponse.js";
 
 
-const adminOnlyMiddleware = async (req, res, next) => {
-  try {
-    const userId = req.userId;
-    const user = await userService.getUserById(userId);
-
-    if (!user) {
-      return res.status(404).json({ success: false, error: "User not found" });
-    }
-
-    if (user.role !== "admin") {
-      return res.status(403).json({ success: false, error: "Access denied. Admin only." });
-    }
-
-    req.user = user;
-    next();
-  } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
-  }
-};
-
 // middleware for authenticate user, when he/she/other forget their password
 const authenticateUserForForgetPassword = async (req, res, next) => {
   try {
@@ -60,7 +40,6 @@ const OTPVerifyMiddleware = async (req, res, next) => {
 
 // exports
 export {
-  adminOnlyMiddleware,
   authenticateUserForForgetPassword,
   OTPVerifyMiddleware
 };
